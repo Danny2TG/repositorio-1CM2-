13649ee7cab1303665ab00cb798ce5e58f060429
Danny <Danny@DESKTOP-AR89GIM>
2020-11-25 23:29:37 -0600

    Repositorio 1CM2

---
+++ arreglo_Scaf_min_y_max.c
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   arreglo_Scaf_min_y_max.c
+ * Author: Danny
+ *
+ * Created on 25 de noviembre de 2020, 09:50 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) { 
+    
+ int x; 
+    int  y;  
+     printf("\n Ingresa el valor de tu arreglo: ");
+    scanf("%d", &x); 
+    float arreglo[x]; 
+    
+      for(y=0;y<x;y++){ 
+        int  z; 
+        printf("\n Ingresa un valor: ");
+        scanf("%d", &z); 
+        arreglo[y]=z;
+    
+    } 
+	float max = arreglo[0];
+	float min = arreglo[0];
+	
+	for(y=0;y<x;y++){ 
+		if (arreglo[y]>max){ 
+			 max = arreglo[y];                
+		} 
+		if (arreglo[y]<min){ 
+			 min = arreglo[y]; 
+			
+		}
+	}
+	printf(" \n el valor minimo es: %f ", min); 
+	printf(" \n el valor maximo es: %f", max);
+                
+    return (EXIT_SUCCESS);
+}
+
